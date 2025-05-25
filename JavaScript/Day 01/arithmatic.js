fd11bf300b8044a3500004788d26888ae930acd1
MohdAman-tech-dot <mohdaman84410@gmail.com>
2025-05-25 22:25:17 +0530

    Folder Added

---
+++ JavaScript/Day 01/arithmatic.js
@@ -0,0 +1,27 @@
+// Addition
+var a = 15;
+var b = 10;
+var sum = a + b;
+console.log(sum)
+
+// Substraction
+var p = 15;
+var q = 3;
+console.log(p-q)
+
+
+// Multiplication
+var A = 18;
+var B = 15;
+console.log(A*B)
+
+// Division
+var Q = 18;
+var P = 15;
+console.log(Q/P)
+
+// Modulus
+var S = 18;
+var R = 15;
+console.log(S%R)
+
---
+++ JavaScript/Day 02/script.js
@@ -0,0 +1,43 @@
+// LOGICAL OPERATORS
+
+// AND Operator:- both condition must be true otherwise its false
+var a = 18;
+var b = 25;
+var result = a<20 && b>20;
+console.log(result)
+
+// OR Operator:- must be one condition true if both condition false than its false
+var aman = 15
+console.log(aman==18 || aman>18) 
+
+
+// NOT Operator:- its convert the condition in true to false and false to true
+var k = true
+console.log(!k)
+
+
+
+// ASSIGNMENT OPERATOR
+// 1) =    eg. a=b    means  a, b=5   than a = 5
+// 2) +=   eg. a+=b   means  a = a+b
+// 3) -=   eg. a-=b   means  a = a-b
+// 4) *=   eg. a*=b   meand  a = a*b
+// 5) /=   eg. a/=b   means  a = a/b
+// 6) %=   eg. a%=b   means  a = a%b
+// 7) **=  eg. a**=2  means  a = a**2       a ki power(ghaat) 2
+
+// a+b=a   we can write like this in assinment operator   a+=b
+var p = 15;
+var q = 12;
+p+=q                 //we can write like in assingment operator   p = p+q
+console.log(p)
+
+//  m = m*n   eg.  m*=n
+var m = 15;
+var n= 5;
+m*=n;
+console.log(m)
+
+var e = 5;
+e**=3
+console.log(e)
